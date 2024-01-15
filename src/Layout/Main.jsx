95e26b3ebc995135c34416d8a971cba1250e8b48
Mahin Hasan <mahinmtrs@gmail.com>
2024-01-16 04:20:30 +0600

    added router, also added navbar and footer

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer/Footer';
+import Navbar from '../Shared/NavBar/Navbar';
+
+const Main = () => {
+    return (
+        <div className='max-w-screen-xl mx-auto'>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
