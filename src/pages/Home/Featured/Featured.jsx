e7b41bcc86523ed8ba754573ed38a8d2ed5a198f
Mahin Hasan <mahinmtrs@gmail.com>
2024-01-17 05:16:34 +0600

    added menu route,used parallex effect, used react helmet, also added custom hooks to load data dynamically

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -3,12 +3,12 @@ import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import './Featured.css'
 const Featured = () => {
     return (
-        <section className='featured-item text-white pt-8 my-20'>
+        <section className='featured-item  bg-fixed text-white pt-8 my-20'>
             <SectionTitle
                 subHeading="Check It Out"
                 heading="Featured Item"
             ></SectionTitle>
-            <div className='md:flex justify-center items-center pb-20 pt-12 px-36'>
+            <div className='md:flex justify-center items-center bg-stone-500 bg-opacity-40 pb-20 pt-12 px-36'>
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -16,7 +16,7 @@ const Featured = () => {
                     <p>Aug 20, 2024</p>
                     <p className='uppercase'>Where can I get some?</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae labore atque dicta laborum ipsa fugiat architecto molestiae. Ipsum itaque voluptatem sint, sit nesciunt corporis enim! Laborum itaque earum placeat dolorem?</p>
-                    <button className='btn btn-outline'>Order Now</button>
+                    <button className='btn btn-outline border-0 border-b-4 mt-4'>Order Now</button>
                 </div>
             </div>
 
