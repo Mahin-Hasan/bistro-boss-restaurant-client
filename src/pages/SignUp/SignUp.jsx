ad0e57687febed90a5e8903c559bdd228dedafee
Mahin Hasan <mahinmtrs@gmail.com>
2024-01-18 12:16:34 +0600

    added private route, used update sign up user info

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,16 +1,48 @@
+import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { AuthContext } from "../../providers/AuthProvider";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const SignUp = () => {
     const {
         register,
         handleSubmit,
+        reset,
         formState: { errors },
-    } = useForm()
+    } = useForm();
+    const { createUser, updateUserProfile } = useContext(AuthContext);
+    const naviate = useNavigate();
+
 
     const onSubmit = (data) => {
-        console.log(data)
-    }
-    return (
+        console.log(data);
+        createUser(data.email, data.password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                updateUserProfile(data.name, data.photoURL)
+                    .then(() => {
+                        console.log('user profile info updated');
+                        reset();
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: "Users info updated",
+                            showConfirmButton: false,
+                            timer: 1500
+                          });
+                          naviate('/');
+                    })
+                    .catch(error => console.log(error))
+    })
+}
+return (
+    <>
+        <Helmet>
+            <title>Bistro Boss | Sign Up</title>
+        </Helmet>
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
@@ -28,6 +60,13 @@ const SignUp = () => {
                         </div>
                         <div className="form-control">
                             <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="text" placeholder="Photo URL" {...register("photoURL", { required: true })} className="input input-bordered" />
+                            {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
                             <input type="email" placeholder="email" {...register("email", { required: true })} className="input input-bordered" />
@@ -38,21 +77,32 @@ const SignUp = () => {
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" {...register("password", { required: true, minLength: 6, maxLength: 20 })} className="input input-bordered" />
-                            {errors.password && <span className="text-red-600">Not valid</span>}
+                            <input type="password" placeholder="password" {...register("password", {
+                                required: true,
+                                minLength: 6,
+                                maxLength: 20,
+                                pattern: /[^a-zA-Z0-9]/
+                            })} className="input input-bordered" />
+                            {errors.password?.type === 'required' && <span className="text-red-600">Password is required</span>}
+                            {errors.password?.type === 'minLength' && <span className="text-red-600">Password must be 6 characters</span>}
+                            {errors.password?.type === 'maxLength' && <span className="text-red-600">Password must be less than 20 characters</span>}
+                            {errors.password?.type === 'pattern' && <span className="text-red-600">At least one special character</span>}
 
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <input className="btn btn-primary" type="submit" value="Sign Up" />
                         </div>
                     </form>
+                    <p><small>Already Have an  account?<Link to='/login'>Login</Link> </small></p>
+
                 </div>
             </div>
         </div>
-    );
+    </>
+);
 };
 
 export default SignUp;
